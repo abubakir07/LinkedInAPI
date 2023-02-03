2875ee97b977ebe6285900343238a291a3301ce7
abubakir07 <amahamatjanov0071225@gmail.com>
2023-02-03 21:30:40 +0600

    fixed bags

--- utils/permissions.py
+++ utils/permissions.py
@@ -4,3 +4,10 @@ from rest_framework.permissions import BasePermission
 class IsUserOwner(BasePermission):
     def has_object_permission(self, request, view, obj):
         return bool(request.user.id == obj.id)
+
+
+class IsOwner(BasePermission):
+    def has_object_permission(self, request, view, obj):
+        return bool(request.user.id == obj.owner.id)
+
+
